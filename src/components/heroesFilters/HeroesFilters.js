4dba9605377c388752d131475cdbb206d5bfc20a
Evgenij Nekrasov <nekrasovevgenij3@gmail.com>
2024-03-18 04:34:29 +0300

    by using the built-in method for working with promises - createAsyncThunk managed to encapsulate the data and now the logic of everything is in the heroesSlice and filtersSlice files

--- src/components/heroesFilters/HeroesFilters.js
+++ src/components/heroesFilters/HeroesFilters.js
@@ -1,12 +1,9 @@
-import { useHttp } from "../../hooks/http.hook";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import classNames from "classnames";
 
 import {
-   filtersFetching,
-   filtersFetched,
-   filtersFetchingError,
+   fetchFilters,
    activeFilterChanged,
 } from "../heroesFilters/filtersSlice";
 import Spinner from "../spinner/Spinner";
@@ -16,14 +13,9 @@ const HeroesFilters = () => {
       (state) => state.filters
    );
    const dispatch = useDispatch();
-   const { request } = useHttp();
 
    useEffect(() => {
-      dispatch(filtersFetching());
-      request("http://localhost:3001/filters")
-         .then((data) => dispatch(filtersFetched(data)))
-         .catch(() => dispatch(filtersFetchingError()));
-
+      dispatch(fetchFilters());
       // eslint-disable-next-line
    }, []);
 
